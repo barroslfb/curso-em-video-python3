063d74e3092a033a599128237f2cf58cfb95b8c4
Luiz Felipe Barros <luizfelipepab@hotmail.com>
2024-05-12 01:34:57 -0300

    Delete Mundo 3 directory

--- Mundo 3/Aula 16 - Tuplas/ex074.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-tupla = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), )
-
-print(f"Os valores sorteados foram: {tupla}")
-print(f"O maior valor sorteado foi {max(tupla)}")
-print(f"O menor valor sorteado foi {min(tupla)}")
--- Mundo 3/README.md
+++
@@ -1,7 +0,0 @@
-# Python 3 - Mundo 3
-- Tuplas em Python
-- Listas em Python
-- Dicionários em Python
-- Funções em Python
-- Modularização em Python
-- Tratamento de Erros em Python
